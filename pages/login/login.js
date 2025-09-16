b5007b010fe89ba0d117f7ee0f86176b5bfeebe3
xianrenrrr <99616758+xianrenrrr@users.noreply.github.com>
2025-09-16 00:49:27 -0700

    12

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,15 +5,25 @@ Page({
     showAgreementModal: false
   },
 
+  onLoad() {
+    // Restore agreement state from storage
+    const agreed = wx.getStorageSync('terms_agreed') || false
+    this.setData({ agreed })
+  },
+
   // 切换协议同意状态
   toggleAgreement() {
-    this.setData({
-      agreed: !this.data.agreed
-    })
+    const next = !this.data.agreed
+    this.setData({ agreed: next })
+    wx.setStorageSync('terms_agreed', next)
   },
 
   // 手机登录
   handlePhoneLogin() {
+    if (!this.data.agreed) {
+      wx.showToast({ title: '请先同意用户使用协议', icon: 'none' })
+      return
+    }
     // 跳转到手机登录页面
     wx.navigateTo({
       url: '/pages/phone-login/phone-login'
@@ -22,6 +32,10 @@ Page({
 
   // 扫码注册
   handleQRSignup() {
+    if (!this.data.agreed) {
+      wx.showToast({ title: '请先同意用户使用协议', icon: 'none' })
+      return
+    }
     // 跳转到扫码注册页面
     wx.navigateTo({
       url: '/pages/signup/signup'
@@ -48,6 +62,7 @@ Page({
       agreed: true,
       showAgreementModal: false
     })
+    wx.setStorageSync('terms_agreed', true)
     wx.showToast({
       title: '已同意用户协议',
       icon: 'success'
